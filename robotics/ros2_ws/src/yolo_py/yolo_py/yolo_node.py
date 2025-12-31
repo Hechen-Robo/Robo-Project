01bbf50281f5a126cbc0c27a0fe746b5941c9e39
Hechen <hechenwang1993@gmail.com>
2025-12-31 02:06:50 +0100

    Add yolo_py node, change .gitingnore file, model file ignored

---
+++ robotics/ros2_ws/src/yolo_py/yolo_py/yolo_node.py
@@ -0,0 +1,62 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import os
+
+from ultralytics import YOLO # type: ignore
+
+class YoloNode(Node):
+    def __init__(self):
+        super().__init__("yolo_py")
+
+        self.declare_parameter("image_topic", "/camera/image_raw")
+        self.declare_parameter("model", "yolov8n.pt")
+        self.declare_parameter("conf", 0.5)
+        self.declare_parameter("device", "cpu")
+
+        self.image_topic = self.get_parameter("image_topic").get_parameter_value().string_value
+        model_path = self.get_parameter("model").get_parameter_value().string_value
+        self.conf = self.get_parameter("conf").get_parameter_value().double_value
+        self.device = self.get_parameter("device").get_parameter_value().string_value
+
+
+        if os.path.isabs(model_path) and (not os.path.exists(model_path)):
+            raise FileNotFoundError(f"Model file not found: {model_path}")
+       
+        self.model = YOLO(model_path)
+        self.get_logger().info(f"Loaded model from {model_path} on device {self.device}")
+
+        self.bridge = CvBridge()
+        self.pub = self.create_publisher(Image, "/yolo/image", 10)
+        self.sub = self.create_subscription(
+            Image,
+            self.image_topic,
+            self.on_image,
+            10
+        )
+
+    def on_image(self, msg):
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        frame = cv2.flip(frame, 1)
+
+        results = self.model(frame, conf=self.conf, device=self.device, verbose=False)[0]
+        annotated_frame = results.plot()
+
+        annotated_rgb = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+        out_msg = self.bridge.cv2_to_imgmsg(annotated_rgb, encoding='rgb8')
+
+        out_msg.header = msg.header
+        self.pub.publish(out_msg)
+        self.get_logger().info(f"Processed image {frame.shape[1]}x{frame.shape[0]}")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = YoloNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+if __name__ == "__main__":
+    main()
+
